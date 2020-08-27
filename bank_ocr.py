965fd80bd6ba3fea1be019b3816ff95f4d76495a
Serkan Kalay <serkan.kalay@klm.com>
2020-08-27 15:58:03 +0200

    Split digits now deal with an entire input.

--- bank_ocr.py
+++ bank_ocr.py
@@ -3,6 +3,7 @@ from dataclasses import dataclass
 DIGIT_LENGTH = 3
 NUMBER_OF_ROWS = 3
 
+
 @dataclass(frozen=True)
 class Digit:
     first_line: str
@@ -11,8 +12,12 @@ class Digit:
 
 
 def split_digits(input_str: str):
+    lines = input_str.splitlines()[1 : NUMBER_OF_ROWS + 1]
     return [
-        Digit(first_line=line[section_start:section_start+DIGIT_LENGTH], second_line="", third_line="")
-        for line in input_str.splitlines()[0:NUMBER_OF_ROWS]
-        for section_start in range(0, len(line), DIGIT_LENGTH)
+        Digit(
+            first_line=lines[0][section_start : section_start + DIGIT_LENGTH],
+            second_line=lines[1][section_start : section_start + DIGIT_LENGTH],
+            third_line=lines[2][section_start : section_start + DIGIT_LENGTH],
+        )
+        for section_start in range(0, len(lines[0]), DIGIT_LENGTH)
     ]
--- test_bank_ocr.py
+++ test_bank_ocr.py
@@ -1,8 +1,13 @@
 from bank_ocr import Digit, split_digits
 
-ONE_TO_NINE_LINE_1 = "    _  _     _  _  _  _  _ "
+ONE_TO_NINE = """
+    _  _     _  _  _  _  _ 
+  | _| _||_||_ |_   ||_||_|
+  ||_  _|  | _||_|  ||_| _|
+"""
 
 
 def test_split_digits():
-    digits = split_digits(ONE_TO_NINE_LINE_1)
+    digits = split_digits(ONE_TO_NINE)
+    print(digits)
     assert len(digits) == 9 and all(isinstance(d, Digit) for d in digits)
